f2ee8c5baaf57e712cf1140fde33f35b54d06883
vasil dervenski <vasil_d@mail.bg>
2020-11-26 12:30:05 +0200

    Outout to header

--- ch2-plugin-header.php
+++ ch2-plugin-header.php
@@ -2,9 +2,25 @@
 /* 
 Plugin Name: Chapter 2 - Plugin Header 
 Plugin URI: 
-Description: Declares a plugin that will be visible in the WordPress admin interface
+Description: Plugin tutorial
 Version: 1.0 
-Author: Yannick Lefebvre 
+Author: Vasil Dervenski 
 Author URI: http://ylefebvre.ca 
 License: GPLv2 
 */
+
+add_action( 'wp_head', 'ch2pho_page_header_output' );
+
+function ch2pho_page_header_output() { ?>
+    <script>
+        (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;
+        i[r]=i[r]||function(){
+        (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();
+        a=s.createElement(o),
+        m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;
+        m.parentNode.insertBefore(a,m)})(window,document,'script',
+        'https://www.google-analytics.com/analytics.js','ga');
+        ga('create', 'UA-0000000-0', 'auto');
+        ga('send', 'pageview');
+    </script> 
+<?php }
